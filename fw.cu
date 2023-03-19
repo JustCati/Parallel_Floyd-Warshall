5a07b0f19e93b5cdb67287bebb2312e53052c422
SO4REN <ale.catinello@gmail.com>
2023-03-19 20:40:08 +0100

    feat: implement Write to file generic matrix function
    WIP: Use the function for chaching check for solution and implement verify function

--- fw.cu
+++ fw.cu
@@ -1,11 +1,26 @@
-#include "GraphCPU/graphCPU.hpp"
-#include "GraphCUDA/graphCuda.cuh"
 #include <iostream>
+#include <string>
+#include <fstream>
+
+#include "Graph/graph.hpp"
+#include "Cuda/CudaFunctions.cuh"
 
 
 void err(const char *msg){
     std::cout << msg << std::endl;
     exit(1);
+}
+
+void writeToFile(const int* matrix, int num, std::string filename){
+    std::ofstream out(filename);
+
+    for(int i = 0; i < num; i++){
+        for(int j = 0; j < num; j++)
+            matrix[i * num + j] == INT_MAX >> 1 ? out << "INF" << " " : out << matrix[i * num + j] << "\t";
+        out << std::endl;
+    }
+
+    out.close();
 }
 
 int main(int argc, char **argv){
@@ -15,21 +30,26 @@ int main(int argc, char **argv){
         err("Utilizzo comando: ./fw num_vertices percentage (0 < percentage < 100)");
 
     int p = atoi(argv[2]);
-    GraphCPU *g = new GraphCPU(atoi(argv[1]), p);
+    Graph* g = new Graph(atoi(argv[1]), p);
 
-    for (int i = 0; i < g->getNumVertices(); i++){
-        for (int j = 0; j < g->getNumVertices(); j++)
-            if(g->adjMatrix[i * g->getNumVertices() + j] == INT_MAX >> 1)
-                std::cout << "INF\t";
-            else
-                std::cout << g->adjMatrix[i * g->getNumVertices() + j] << "\t";
-        std::cout << std::endl;
-    }
+    writeToFile(g->getAdjMatrix(), g->getNumVertices(), "graph.txt");
 
-    //! ------------ TEST FLOYD WARSHALL CPU--------------
-    int *d = FloydWarshallCPU(*g);
-    delete[] d;
+    //! ------------ FLOYD WARSHALL CPU--------------
+
+    int *d_CPU = FloydWarshallCPU(*g);
+    writeToFile(d_CPU, g->getNumVertices(), "fw_cpu.txt");
+    delete[] d_CPU;
+
     //! --------------------------------------------------
+
+
+    //! ------------ SIMPLE FLOYD WARSHALL GPU --------------
+
+    int* d_GPU = simple_parallel_FW(*g);
+    writeToFile(d_GPU, g->getNumVertices(), "fw_gpu_simple.txt");
+    cuda(cudaFreeHost(d_GPU));
+
+    //! ----------------------------------------------------------
 
     delete g;
     exit(0);
